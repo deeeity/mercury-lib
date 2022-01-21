be09007e4acc6e163cbe3ce827a2345fc1df2bac
AbstractPoo <61809264+AbstractPoo@users.noreply.github.com>
2022-01-21 23:44:12 +0000

    Update src.lua

--- src.lua
+++ src.lua
@@ -886,7 +886,7 @@ function Library:create(options)
 	rawset(mt, "creditsContainer", creditsTab.container)
 	
 	creditsTab:credit{Name = "Abstract", Description = "UI Library Developer", Discord = "Abstract#8007", V3rmillion = "AbstractPoo"}
-	creditsTab:credit{Name = "Deity", Description = "UI Library Developer", Discord = "Deity#0228", V3rmillion = "Deity"}
+	creditsTab:credit{Name = "Deity", Description = "UI Library Developer", Discord = "Deity#0228", V3rmillion = "0xDEITY"}
 	
 	return mt
 end
