932e0e78786e99a32a9f3d2ad7464e4977f5c003
AbstractPoo <61809264+AbstractPoo@users.noreply.github.com>
2022-04-02 23:59:39 +0100

    Sections added
    Added sections
    tab:section{
         Name = "Section" -- default
    }

--- src.lua
+++ src.lua
@@ -437,14 +437,14 @@ function Library:create(options)
 		Theme = self.Themes[settings.Theme],
 		Link = "https://github.com/deeeity/mercury-lib"
 	}, options)
-	
+
 	if getgenv and getgenv().MercuryUI then
 		getgenv():MercuryUI()
 		getgenv().MercuryUI = nil
 	end
 
-	
-	
+
+
 	if options.Link:sub(-1, -1) == "/" then
 		options.Link = options.Link:sub(1, -2)
 	end
@@ -578,7 +578,7 @@ function Library:create(options)
 	closeButton.MouseLeave:connect(function()
 		closeButton:tween{ImageColor3 = Library.CurrentTheme.StrongText}
 	end)
-	
+
 	local function closeUI()
 		core.ClipsDescendants = true
 		core:fade(true)
@@ -587,11 +587,11 @@ function Library:create(options)
 			gui.AbsoluteObject:Destroy()
 		end)
 	end
-	
+
 	if getgenv then
 		getgenv().MercuryUI = closeUI
 	end
-		
+
 	closeButton.MouseButton1Click:connect(function()
 		closeUI()
 	end)
@@ -1275,7 +1275,11 @@ function Library:tab(options)
 end
 
 function Library:_resize_tab()
-	self.container.CanvasSize = UDim2.fromOffset(0, self.layout.AbsoluteContentSize.Y + 20)
+	if self.container.ClassName == "ScrollingFrame" then
+		self.container.CanvasSize = UDim2.fromOffset(0, self.layout.AbsoluteContentSize.Y + 20)
+	else
+		self.sectionContainer.Size = UDim2.new(1, -20, 0, self.layout.AbsoluteContentSize.Y + 20)
+	end
 end
 
 function Library:toggle(options)
@@ -1713,6 +1717,55 @@ function Library:dropdown(options)
 	end
 
 	return methods
+end
+
+function Library:section(options)
+	options = self:set_defaults({
+		Name = "Section"
+	}, options)
+
+	local sectionContainer = self.container:object("TextButton", {
+		BackgroundTransparency = 1,
+		Size = UDim2.new(1, -20, 0, 52)
+	}):round(7):stroke("Secondary", 2)
+	
+	local text = sectionContainer:object("TextLabel", {
+		Position = UDim2.new(0.5),
+		Text = options.Name,
+		TextSize = 18,
+		Theme = {
+			TextColor3 = "StrongText",
+			BackgroundColor3 = {"Secondary", -10}
+		},
+		TextXAlignment = Enum.TextXAlignment.Center,
+		AnchorPoint = Vector2.new(0.5, 0.5)
+	})
+	text.Size = UDim2.fromOffset(text.TextBounds.X + 4, text.TextBounds.Y)
+	
+	
+	local functionContainer = sectionContainer:object("Frame", {
+		Size = UDim2.fromScale(1, 1),
+		BackgroundTransparency = 1
+	})
+	
+	
+	local layout = functionContainer:object("UIListLayout", {
+		Padding = UDim.new(0, 10),
+		HorizontalAlignment = Enum.HorizontalAlignment.Center
+	})
+
+	functionContainer:object("UIPadding", {
+		PaddingTop = UDim.new(0, 10)
+	})
+	
+	return setmetatable({
+		statusText = self.statusText,
+		container = functionContainer,
+		sectionContainer = sectionContainer,
+		Theme = self.Theme,
+		core = self.core,
+		layout = layout
+	}, Library)
 end
 
 function Library:button(options)
