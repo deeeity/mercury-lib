ffe05aaf2e922fd0749cd17b89c4d6c117bc5abc
realnyz <102926073+realnyz@users.noreply.github.com>
2022-05-29 15:12:48 +0200

    change logo trol
    changed logo

--- src.lua
+++ src.lua
@@ -689,7 +689,7 @@ function Library:create(options)
 		BackgroundTransparency = 1,
 		Position = UDim2.new(0, 5, 0.5, 0),
 		Size = UDim2.new(0, 15, 0, 15),
-		Image = "http://www.roblox.com/asset/?id=8569322835",
+		Image = "http://www.roblox.com/asset/?id=9760082512",
 		Theme = {ImageColor3 = "StrongText"}
 	})
 
